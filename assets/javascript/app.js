fd829b417836654c9ceeaad06c9c0e936ae29e21
ANS <36783010+answebdev@users.noreply.github.com>
2018-09-23 20:42:44 -0700

    connected to Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
 // alert("Choo Choo. Chee Chee.");
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCwUY21eQZK8mwi7h9Ibq4N5QoS7K-q-l4",
+  authDomain: "trainscheduler-6b06e.firebaseapp.com",
+  databaseURL: "https://trainscheduler-6b06e.firebaseio.com",
+  projectId: "trainscheduler-6b06e",
+  storageBucket: "trainscheduler-6b06e.appspot.com",
+  messagingSenderId: "853801615807"
+};
+
+firebase.initializeApp(config);
+
+// var database = firebase.database();
+// console.log(database);
