9b04ceffacd07c2a51a9462817eb50641948f755
ANS <36783010+answebdev@users.noreply.github.com>
2018-09-24 15:56:33 -0700

    initial logic added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,7 @@ var destination = "";
 var firstTrainTime = "";
 var frequency = 0;
 
+// Grab user input
 $("#submit").on("click", function () {
   trainName = $("#trainInput").val().trim();
   destination = $("#destinationInput").val().trim();
@@ -30,25 +31,76 @@ $("#submit").on("click", function () {
   console.log(firstTrainTime);
   console.log(frequency);
 
-  // Because we have a type="submit" button, it's going to reload the page
-  // whenever we click it. So, we need "return false" so that it won't
-  // reload the page whenever we click submit.
-  // return false;
-
+  // Upload train data to the database
   firebase.database().ref().push({
     trainName: trainName,
     destination: destination,
     firstTrainTime: firstTrainTime,
     frequency: frequency
-
   })
+  // Clears all of the text-boxes
+  $("#trainInput").val("");
+  $("#destinationInput").val("");
+  $("#timeInput").val("");
+  $("#frequencyInput").val("");
+  // Because we have a type="submit" button, it's going to reload the page
+  // whenever we click it. So, we need "return false" so that it won't
+  // reload the page whenever we click submit
   return false;
 })
 
-firebase.database().ref().on("value", function (snapshot) {
-  $("#name-display").html(snapshot.val().trainName);
-  $("#destination-display").html(snapshot.val().destination);
-  $("#frequency-display").html(snapshot.val().firstTrainTime);
-  $("#arrival-display").html(snapshot.val().frequency);
-  // $("#minutes-display").html(snapshot.val().name);
-})
+
+// firebase.database().ref().on("value", function (snapshot) {
+//   $("#name-display").html(snapshot.val().trainName);
+//   $("#destination-display").html(snapshot.val().destination);
+//   $("#frequency-display").html(snapshot.val().firstTrainTime);
+//   $("#arrival-display").html(snapshot.val().frequency);
+//   // $("#minutes-display").html(snapshot.val().name);
+// })
+
+
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// firebase.database.ref().on("child_added", function(childSnapshot) {
+//   console.log(childSnapshot.val());
+
+//   // Store everything into a variable.
+//   var train = childSnapshot.val().trainName;
+//   var dest = childSnapshot.val().destination;
+//   var time = childSnapshot.val().firstTrainTime;
+//   var freq = childSnapshot.val().frequency
+
+//   // Employee Info
+//   console.log(train);
+//   console.log(dest);
+//   console.log(time);
+//   console.log(freq);
+// });
+
+
+
+//   // Prettify the employee start
+//   var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+
+//   // Calculate the months worked using hardcore math
+//   // To calculate the months worked
+//   var empMonths = moment().diff(moment(empStart, "X"), "months");
+//   console.log(empMonths);
+
+//   // Calculate the total billed rate
+//   var empBilled = empMonths * empRate;
+//   console.log(empBilled);
+
+//   // Create the new row
+//   var newRow = $("<tr>").append(
+//     $("<td>").text(empName),
+//     $("<td>").text(empRole),
+//     $("<td>").text(empStartPretty),
+//     $("<td>").text(empMonths),
+//     $("<td>").text(empRate),
+//     $("<td>").text(empBilled)
+//   );
+
+//   // Append the new row to the table
+//   $("#employee-table > tbody").append(newRow);
+// });
